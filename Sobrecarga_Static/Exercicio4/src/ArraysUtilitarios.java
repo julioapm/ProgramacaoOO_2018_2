0ae862936b6d74b26e98516afa049346d6167ad2
Júlio Pereira Machado <julioapm@gmail.com>
2018-08-14 23:28:38 -0300

    Commit inicial

---
+++ Sobrecarga_Static/Exercicio4/src/ArraysUtilitarios.java
@@ -0,0 +1,44 @@
+/**
+ * Esta classe fornece métodos utilitários para a manipulação de array de inteiros.
+ */
+public class ArraysUtilitarios {
+    /**
+     * Realiza o somatório de todos os elementos no array a.
+     * @param a um array de inteiros
+     * @return o somatório de todos os elementos no array a
+     */
+    public static int somar(int[] a) {
+
+    }
+
+    /**
+     * Realiza o somatório dos elementos no array a no intervalo [i;f].
+     * @param a um array de inteiros
+     * @param i inteiro representando o índice inferior do intervalo
+     * @param f inteiro representando o índice superior do intervalo
+     * @return o somatório de todos os elementos no array a no intervalo [i;f]
+     */
+    public static int somar(int[] a, int i, int f) {
+
+    }
+
+    /**
+     * Busca a posição da primeira ocorrência do elemento x no array a.
+     * @param a um array de inteiros
+     * @param x um inteiro
+     * @return o índice da primeira ocorrência de x no array a ou -1 caso x não esteja no array
+     */
+    public static int buscar(int[] a, int x) {
+
+    }
+
+    /**
+     * Busca a posição da primeira ocorrência do elemento x no array ordenado a.
+     * @param a um array de inteiros ordenado em ordem crescente
+     * @param x um inteiro
+     * @return o índice da primeira ocorrência de x no array a ou -1 caso x não esteja no array
+     */
+    public static int buscarOrdenado(int[] a, int x) {
+
+    }
+}
